94b6a3eb2557c6c042e7cf39ded45c51888e3398
tgualchi <97109000+tgualchi@users.noreply.github.com>
2023-02-02 11:48:48 -0300

    separe las carpetas Back y Front

---
+++ Proyecto_TG/BackEnd/src/main/java/com/porfolio/tob/GualApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.tob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GualApplication.class, args);
+	}
+
+}
---
+++ Proyecto_TG/BackEnd/src/test/java/com/porfolio/tob/GualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.tob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
